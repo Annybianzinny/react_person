d35e31e28d09187a69f2a41079ceb3e6404c7df2
Anik <anikferreira8@gmail.com>
2026-07-02 20:48:53 -0300

    Implement Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <div className="Person">
+      <p className="Person__name">{name}</p>
+
+      {age !== undefined && <p className="Person__age">{age}</p>}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${sex === 'm' ? 'wife' : 'husband'}: ${partnerName}`
+          : 'I am not married'}
+      </p>
+    </div>
+  );
+};
